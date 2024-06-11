1dbdf29b058f308402d18c35501e8b35a985af5b
FitsumMehari <m.fitsummehari@gmail.com>
2024-06-11 15:25:45 +0300

    add route for authentication and authorization

---
+++ api/routes/auth.js
@@ -0,0 +1,68 @@
+const router = require('express').Router();
+const cryptoJS = require('crypto-js');
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
+const md5 = require("md5");
+
+dotenv.config();
+
+// const User = require('../models/User');
+
+
+//REGISTER USER
+router.post('/register', async(req, res, next) => {
+    if (!req.body.username || !req.body.password || !req.body.email) {
+        res.status(400).json("Please fill the required inputs!")
+    } else {
+        const newUser = new User({
+            username: req.body.username,
+            password: md5(req.body.password),
+            email: req.body.email
+        })
+        try {
+            const savedUser = await newUser.save();
+
+            const { password, ...otherUserInfo } = savedUser._doc
+            res.status(201).json({ message: "Account Created Successfully!", otherUserInfo });
+        } catch (err) {
+            return next(err);
+        }
+    }
+})
+
+
+//LOGIN
+router.post('/login', async(req, res) => {
+    if (!req.body.email || !req.body.password) {
+        res.status(400).json("Please fill the required inputs!")
+    } else {
+        try {
+            const user = await User.findOne({ email: req.body.email });
+
+            // !user && res.status(401).json("Wrong Credientials!");
+            if (!user) {
+                return res.status(401).json("Wrong Credientials!");
+            }
+
+            const hashedPassword = md5(req.body.password);
+            const userPassword = user.password
+
+            // userPassword !== req.body.password && res.status(401).json("Wrong Credientials!");
+            if (userPassword !== hashedPassword) {
+                return res.status(401).json("Wrong Credientials!");
+            }
+
+            const accessToken = jwt.sign({
+                id: user._id
+            }, process.env.JWTKEY, { expiresIn: '7d' });
+
+            const { password, ...userDetails } = user._doc;
+
+            res.status(200).json({ message: "Log In Successful!", userDetails, accessToken });
+        } catch (err) {
+            return next(err);
+        }
+    }
+})
+
+module.exports = router;
