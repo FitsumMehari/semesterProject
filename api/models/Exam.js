e4c40a339b18bdaf54487c9bd94d27dfc3b5969e
FitsumMehari <m.fitsummehari@gmail.com>
2024-06-11 16:35:32 +0300

    add exam db schema

---
+++ api/models/Exam.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const ExamSchema = new mongoose.Schema({
+    examTitle: {
+        type: String,
+        required: true,
+    },
+    courseName: {
+        type: String,
+        required: true,
+    },
+    Questions: [{
+        question: String,
+        choices: {
+            a: String,
+            b: String,
+            c: String,
+            d: String,
+        },
+        answer: String,
+    }, ],
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model("exam", ExamSchema);
