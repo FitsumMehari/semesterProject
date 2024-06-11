971a2137d2fdd7d314ce3f65bbf0ee2c0f7ccb84
FitsumMehari <m.fitsummehari@gmail.com>
2024-06-11 17:05:40 +0300

    add route to add exam

--- api/routes/exam.js
+++ api/routes/exam.js
@@ -7,12 +7,30 @@ const md5 = require("md5");
 dotenv.config();
 
 const Exam = require('../models/Exam');
+const { verifyTokenAndAuthorization } = require('./verifyToken');
 
 // Get all exams
 router.get('/all', async(req, res, next) => {
     try {
         const exam = await Exam.find()
         res.status(200).json(exam);
+    } catch (error) {
+        next(error)
+    }
+})
+
+// Add exam
+router.post('/:userId', verifyTokenAndAuthorization, async(req, res, next) => {
+    try {
+
+        const newExam = new Exam({
+            courseName: req.body.courseName,
+            examTitle: req.body.examTitle,
+            Questions: req.body.questions,
+        })
+
+        const savedExam = await newExam.save();
+        res.status(201).json(savedExam)
     } catch (error) {
 
     }
