c1981d74cb4f6593497877b087715ad72ae27384
FitsumMehari <m.fitsummehari@gmail.com>
2024-06-11 15:21:10 +0300

    add route for error and route not found

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
-const express = require("experss");
+const express = require("express");
 
 const PORT = 3000;
 const app = express();
 app.use(express.json());
+
+
+//ROUTE NOT FOUND
+app.use((req, res, next) => {
+    res.status(404).send("Sorry, route could not be located!");
+});
+
+//ERROR
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+});
 
 app.listen(PORT, () => {
     console.log(`Server is up and running on PORT: ${PORT}`);
