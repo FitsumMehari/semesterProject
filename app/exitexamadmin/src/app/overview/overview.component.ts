a3bb29055d28b2aafdc1b17ec8c6dd60150c4ffa
FitsumMehari <m.fitsummehari@gmail.com>
2024-08-06 13:47:36 +0300

    adjust icons, and get data from db

--- app/exitexamadmin/src/app/overview/overview.component.ts
+++ app/exitexamadmin/src/app/overview/overview.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { MaterialService } from '../material.service';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
-  styleUrl: './overview.component.css'
+  styleUrl: './overview.component.css',
 })
 export class OverviewComponent implements OnInit {
- ngOnInit(): void {
-     console.log("Page");
+  constructor(private userService: UserService, private materialService: MaterialService) {}
 
- }
+  users: any
+  materials: any
+
+  ngOnInit(): void {
+    this.userService.getAllUsers().subscribe(
+      (next) => {
+        this.users = next;
+      },
+      (error) => {}
+    );
+    this.materialService.getAllMaterials().subscribe(
+      (next) => {
+        this.materials = next;
+      },
+      (error) => {}
+    );
+  }
 }
